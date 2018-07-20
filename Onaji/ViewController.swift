b6474f4bf2e52c59a4c956bd0d472cba4cd497bd
MattBurzec <mburzec@cps.edu>
2018-07-20 09:12:09 -0700

    Initial Commit

---
+++ Onaji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onaji
+//
+//  Created by Matthew  Burzec on 7/20/18.
+//  Copyright © 2018 Matthew Burzec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
